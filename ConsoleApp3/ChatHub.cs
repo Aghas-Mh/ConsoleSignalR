98145b10433e01c7fce04f10f3cc57b4059f830e
Aghas-Mh <aghasi.mheryan.2001@gmail.com>
2023-11-24 19:14:44 +0400

    Startup

---
+++ ConsoleApp3/ChatHub.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    public class ChatHub : Hub
+    {
+        public ChatHub()
+        {
+            Console.WriteLine("Startup");
+        }
+
+        Random rand = new Random();
+        public async Task SendMessage(string user, string message)
+        {
+            await Console.Out.WriteLineAsync("Testing");
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public async Task GetID(string user)
+        {
+            Console.WriteLine("GET ID CALLED");
+            int id = rand.Next();
+            await Clients.Caller.SendAsync("ReciveID", id);
+        }
+
+        public async override Task OnConnectedAsync()
+        {
+            await Console.Out.WriteLineAsync("New Connection");
+            var user = Context.GetHttpContext().User;
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ConsoleApp3;
+using Microsoft.AspNetCore.Cors.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//builder.Services.AddRazorPages();
+builder.Services.AddSignalR();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy",
+        builder => builder
+            .AllowAnyMethod()
+            .AllowAnyHeader()
+            .SetIsOriginAllowed(origin => true) // Разрешить любой источник
+            .AllowCredentials());
+});
+
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseCors("CorsPolicy");
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+//app.MapRazorPages();
+app.MapHub<ChatHub>("/chatHub");
+
+app.Run();
